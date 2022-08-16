f80d31b802193ed6dc530384d8247c2b75178ee6
Tien Phuc <leesinvn123@gmail.com>
2022-08-16 21:55:40 +0700

    Test the tialwind config and deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import "./App.css";
 
 function App() {
-  return <div>Hello World</div>;
+  return <div className="flex justify-center">Hello World</div>;
 }
 
 export default App;
